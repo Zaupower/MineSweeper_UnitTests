f6252b9fccfd6fac91de70b22b8ad4fa57273eb8
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-29 16:41:06 +0100

    Define test cases for Open() method

---
+++ Minesweeper/Minesweeper.CoreTests/GameProcessorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Core.Tests
+{
+    [TestClass()]
+    public class GameProcessorTests
+    {
+        [TestMethod()]
+        public void GameProcessorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
