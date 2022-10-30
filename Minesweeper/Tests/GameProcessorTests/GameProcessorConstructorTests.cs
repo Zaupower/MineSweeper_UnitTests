f3d46b69e3d5567439405e01f3b3c5bc06472562
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-30 14:06:15 +0000

    GameProcessor_Constructor tests

--- Minesweeper/Tests/GameProcessorTests/GameProcessorConstructorTests.cs
+++ Minesweeper/Tests/GameProcessorTests/GameProcessorConstructorTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Minesweeper.Core.Enums;
+using Minesweeper.Core.Models;
+using Minesweeper.Core;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,37 @@ using System.Threading.Tasks;
 
 namespace Tests.GameProcessor
 {
+    [TestFixture]
     internal class GameProcessorConstructorTests
     {
+
+        private GameSettings? _settings;
+        private bool[,]? _field;
+        private Minesweeper.Core.GameProcessor? _gameProcessor;
+        private PointState[,]? _currentField;
+        
+        [Test]
+        public void GameProcessor_GameProcessorObject_Equal([Values(0, 1, 2)] int difficultyLevel)
+        {
+            //Setup before every test
+            _settings = DifficultyManager.GetGameSettingsByDifficultylevel((DifficultyLevel)difficultyLevel);
+
+            _field = FieldGenerator.GetRandomField(_settings.Height, _settings.Width, _settings.Mines);
+
+            _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
+            Assert.IsInstanceOf<Minesweeper.Core.GameProcessor>(_gameProcessor);
+        }
+
+        [Test]
+        public void GameProcessor_RectangularFieldGameProcessorObject_Equal([Values(0, 1, 2)] int difficultyLevel, [Values(5, 10)] int rowLenght, [Values(10,5)] int columnLenght)
+        {
+            //Setup before every test
+            _settings = DifficultyManager.GetGameSettingsByDifficultylevel((DifficultyLevel)difficultyLevel);
+
+            _field = FieldGenerator.GetRandomField(rowLenght, columnLenght, _settings.Mines);
+
+            _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
+            Assert.IsInstanceOf<Minesweeper.Core.GameProcessor>(_gameProcessor);
+        }
     }
 }
