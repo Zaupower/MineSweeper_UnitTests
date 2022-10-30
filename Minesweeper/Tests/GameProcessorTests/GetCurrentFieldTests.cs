2b503618c14132ea9ef4eb37130ba8a0d5323812
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-30 14:32:03 +0000

    GameProcessor_Constructor tests

--- Minesweeper/Tests/GameProcessorTests/GetCurrentFieldTests.cs
+++ Minesweeper/Tests/GameProcessorTests/GetCurrentFieldTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Minesweeper.Core.Enums;
+using Minesweeper.Core.Models;
+using Minesweeper.Core;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,27 @@ using System.Threading.Tasks;
 
 namespace Tests.GameProcessor
 {
+    [TestFixture]
     internal class GetCurrentFieldTests
     {
+        private GameSettings? _settings;
+        private bool[,]? _field;
+        private Minesweeper.Core.GameProcessor? _gameProcessor;
+        private PointState[,]? _currentField;
+
+
+        //Verify if all publicFieldInfo.
+        [Test]
+        public void GetCurrentField_GameProcessorObject_Equal([Values(0, 1, 2)] int difficultyLevel)
+        {
+            //Setup before every test
+            _settings = DifficultyManager.GetGameSettingsByDifficultylevel((DifficultyLevel)difficultyLevel);
+
+            _field = FieldGenerator.GetRandomField(_settings.Height, _settings.Width, _settings.Mines);
+
+            _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
+            Assert.IsInstanceOf<Minesweeper.Core.GameProcessor>(_gameProcessor);
+        }
+
     }
 }
