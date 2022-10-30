39a56468c9d5023bd41ee8f88fb9e45d5336c22e
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-30 15:33:29 +0000

    GameProcessor_Constructor tests

--- Minesweeper/Tests/GameProcessorTests/GetCurrentFieldTests.cs
+++ Minesweeper/Tests/GameProcessorTests/GetCurrentFieldTests.cs
@@ -20,16 +20,66 @@ namespace Tests.GameProcessor
 
 
         //Verify if all publicFieldInfo.
+        //Missing test all field is equaly returned (_field[x,y] == _currentField[x,y]
         [Test]
         public void GetCurrentField_GameProcessorObject_Equal([Values(0, 1, 2)] int difficultyLevel)
         {
-            //Setup before every test
+            GameState expectedGameState;
+            GameState actualGameState;
+            int x=0, y = 0;
+
+
             _settings = DifficultyManager.GetGameSettingsByDifficultylevel((DifficultyLevel)difficultyLevel);
-
             _field = FieldGenerator.GetRandomField(_settings.Height, _settings.Width, _settings.Mines);
-
             _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
-            Assert.IsInstanceOf<Minesweeper.Core.GameProcessor>(_gameProcessor);
+            actualGameState = _gameProcessor.Open(x, y);
+
+            _currentField = _gameProcessor.GetCurrentField();
+
+            switch (actualGameState)
+            {
+                case GameState.Active:
+                    for (int row = 0; row < _settings.Height; row++)
+                    {
+                        for (int column = 0; column < _settings.Height; column++)
+                        {
+                            var fieldCell = _field[row, column];
+                            var currentFieldCell = _currentField[row, column];
+                            if (row == x && column == y)
+                            {
+                                Assert.AreNotEqual(currentFieldCell, PointState.Mine, "Correct state of cell(not mine) opened");
+                            }
+                            //else
+                            //{
+                            //    Assert.AreEqual(currentFieldCell, fieldCell? PointState.Close : PointState.Mine, "Same state has previous");
+                            //}
+                        }
+                    }
+                    break;
+                case GameState.Lose:
+                    for (int row = 0; row < _settings.Height; row++)
+                    {
+                        for (int column = 0; column < _settings.Height; column++)
+                        {
+                            var fieldCell = _field[row, column];
+                            var currentFieldCell = _currentField[row, column];
+                            if (row == x && column == y)
+                            {
+                                Assert.AreEqual(currentFieldCell, PointState.Mine, "Correct state of cell opened, mine");
+                            }
+                            //else
+                            //{
+                            //    Assert.AreEqual(currentFieldCell, fieldCell ? PointState.Close : PointState.Mine, "Same state has previous");
+                            //}
+                        }
+                    }
+                    break;
+                //case GameState.Win:
+
+                  //  break;
+            }
+            
+            
         }
 
     }
