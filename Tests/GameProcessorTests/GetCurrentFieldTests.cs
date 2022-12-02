d38af3e32893cc5b3b72cbb905c21472ac46d13a
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-12-02 18:02:43 +0000

    add helper class to generate costum field

--- Tests/GameProcessorTests/GetCurrentFieldTests.cs
+++ Tests/GameProcessorTests/GetCurrentFieldTests.cs
@@ -12,7 +12,7 @@ using Tests.GameProcessorTests.Helper;
 namespace Tests.GameProcessor
 {
     [TestFixture]
-    internal class GetCurrentFieldTests
+    public class GetCurrentFieldTests
     {
         private GameSettings? _settings;
         private bool[,]? _field;
@@ -71,9 +71,7 @@ namespace Tests.GameProcessor
         [TestCase(6, 6)]
         [TestCase(6, 7)]
         public void GetCurrentField_AllCellsAreClosed_BeforeOpen(int rowLenght, int columnLenght)
-        {            
-            GameState actualGameState;
-
+        {  
             _field = genCostumField.generateField(rowLenght, columnLenght, minesIndexs);
             _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
             //actualGameState = _gameProcessor.Open(x, y);
@@ -95,11 +93,8 @@ namespace Tests.GameProcessor
         [TestCase(6, 7)]
         public void GetCurrentField_PointStateNeighbors_AllNeighborsAreCorrect(int rowLenght, int columnLenght)
         {
-            GameState actualGameState;
-            int pointStates = 8;
             _field = genCostumField.generateField(rowLenght, columnLenght, minesIndexs);
             _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
-            //actualGameState = _gameProcessor.Open(x, y);
 
             _currentField = _gameProcessor.GetCurrentField();
 
@@ -113,81 +108,8 @@ namespace Tests.GameProcessor
                 Assert.AreEqual(currentPointState, cell.Item3, $"Correct state of cell {cell.Item2}, {cell.Item1}");
             }
         }
+
+        //Falta contar o numero de minas depois de ganhar e perder
         
-        //Falta verificar se a celula aberta e mina
-        [Test]
-        public void GetCurrentField_RectangularField_Equal([Values(0, 1, 2)] int difficultyLevel, [Values(5, 10)] int rowLenght, [Values(10, 5)] int columnLenght)
-        {
-            GameState actualGameState;
-            int x = 0, y = 0;
-
-            _settings = DifficultyManager.GetGameSettingsByDifficultylevel((DifficultyLevel)difficultyLevel);
-            _field = FieldGenerator.GetRandomField(rowLenght, columnLenght, _settings.Mines);
-            _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
-            actualGameState = _gameProcessor.Open(x, y);
-
-            _currentField = _gameProcessor.GetCurrentField();
-
-            var currentFieldCell = _currentField[x, y];
-
-            switch (actualGameState)
-            {
-                case GameState.Active:
-                    Assert.AreNotEqual(currentFieldCell, PointState.Mine, "Correct state of cell(not mine) opened");
-                    break;
-                case GameState.Lose:
-                    Assert.AreEqual(currentFieldCell, PointState.Mine, "Correct state of cell opened, mine");
-                    break;
-                case GameState.Win:
-                    for (int row = 0; row < rowLenght; row++)
-                    {
-                        for (int column = 0; column < columnLenght; column++)
-                        {
-                            Console.WriteLine(_currentField[row, column]);
-                        }
-                    }
-                    break;
-            }
-        }
-
-
-        [Test]
-        public void GetCurrentField_RectangularFieldNoMines_Equal(
-            [Values(0, 1, 2)] int difficultyLevel, 
-            [Values(5, 10)] int rowLenght, 
-            [Values(10, 5)] int columnLenght)
-        {            
-            GameState actualGameState;
-            int x = 0, y = 0;
-
-
-            _settings = DifficultyManager.GetGameSettingsByDifficultylevel((DifficultyLevel)difficultyLevel);
-            _field = FieldGenerator.GetRandomField(rowLenght, columnLenght, _settings.Mines);
-            _gameProcessor = new Minesweeper.Core.GameProcessor(_field);
-            actualGameState = _gameProcessor.Open(x, y);
-
-            _currentField = _gameProcessor.GetCurrentField();
-
-            var currentFieldCell = _currentField[x, y];
-
-            switch (actualGameState)
-            {
-                case GameState.Active:
-                    Assert.AreNotEqual(currentFieldCell, PointState.Mine, "Correct state of cell(not mine) opened");
-                    break;
-                case GameState.Lose:
-                    Assert.AreEqual(currentFieldCell, PointState.Mine, "Correct state of cell opened, mine");
-                    break;
-                case GameState.Win:
-                    for (int row = 0; row < rowLenght; row++)
-                    {
-                        for (int column = 0; column < columnLenght; column++)
-                        {
-                            Console.WriteLine(_currentField[row, column]);
-                        }
-                    }
-                    break;
-            }
-        }
     }
 }
